1ca9218c5a4c4d5503a648ef49ce49c9526573e4
h3ct04 <hector@pegasconsentido.cl>
2022-06-16 21:28:00 -0400

    Se termian desafio

---
+++ app.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const app = express();
+
+const http = require('http');
+const server = http.createServer(app);
+const io = require('socket.io')(server);
+
+const port = 8080;
+const chat = [];
+const users = [];
+const products = [
+  {
+    name: 'Camara',
+    price: 199000,
+    image:
+      'https://cdn3.iconfinder.com/data/icons/spring-2-1/30/Camera-512.png',
+  },
+];
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+app.set('views', './public/views');
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('pages/index.ejs', { products, title: 'Productos' });
+});
+
+// app.post('/', (req, res) => {
+//   console.log('Body: ', req.body);
+//   products.push({ ...req.body });
+
+//   res.render('pages/index.ejs', { products, title: 'Productos' });
+// });
+
+app.get('/chat', (req, res) => {
+  res.sendFile(__dirname + '/public/chat.html');
+});
+
+io.on('connection', (channel) => {
+  emitChat();
+  sendUsers();
+  emitProduct();
+  channel.on('incomingMessage', (message) => {
+    chat.push(message);
+    if (!users.includes(message.nombre)) users.push(message.nombre);
+    emitChat();
+  });
+  channel.on('addProduct', (product) => {
+    console.log(product);
+    products.push(product);
+    emitProduct();
+  });
+});
+
+const emitChat = () => io.sockets.emit('chat', chat);
+const emitProduct = () => io.sockets.emit('products-inner', products);
+const sendUsers = () => io.sockets.emit('usersList', users);
+
+server.listen(port, () => {
+  console.log(`Escuchando el puerto http://localhost:${port}`);
+});
---
+++ public/index.js
@@ -0,0 +1,101 @@
+// const { io } = require('socket.io-client');
+const socket = io('http://localhost:8080');
+
+const nameInput = document.getElementById('email-input');
+const messageInput = document.getElementById('message');
+const messages = document.getElementById('messages');
+const productName = document.getElementById('product-name');
+const productPrice = document.getElementById('product-price');
+const productImage = document.getElementById('product-image');
+
+function sendProduct() {
+  if (!productName.value && !productPrice.value && !productImage.value) {
+    return;
+  }
+  const product = {
+    name: productName.value,
+    price: productPrice.value,
+    image: productImage.value,
+  };
+
+  socket.emit('addProduct', product);
+  productName.value = '';
+  productPrice.value = '';
+  productImage.value = '';
+}
+
+function sendMessage() {
+  if (!nameInput.value) {
+    return alert('Debe tener un email');
+  }
+  if (!messageInput.value) {
+    return;
+  }
+
+  nameInput.disabled = true;
+  const message = {
+    nombre: nameInput.value,
+    message: messageInput.value,
+  };
+
+  socket.emit('incomingMessage', message);
+  messageInput.value = '';
+  messageInput.focus();
+}
+
+socket.on('chat', (message) => {
+  const dateTime = new Date().toISOString();
+  const [date, time] = dateTime.split('T');
+  const [YYYY, MM, DD] = date.split('-');
+  const [HH, mm, ss] = time.split(':');
+
+  const _dateTime = `${YYYY}/${MM}/${DD} ${HH}:${mm}:${ss.slice(0, 2)}`;
+
+  const texto = message
+    .map(
+      (mensaje) =>
+        `<div> <strong> ${mensaje.nombre} [${_dateTime}] :</strong> <em>${mensaje.message}</em></div>`
+    )
+    .join(' ');
+  document.getElementById('messages').innerHTML = texto;
+});
+
+socket.on('products-inner', (products) => {
+  const headers = [
+    `
+        <tr class="text-light">
+          <th>Nombre</th>
+          <th>Precio</th>
+          <th>Foto URL</th>
+        </tr>
+  `,
+  ];
+  const html = [
+    ...headers,
+    ...products.map(
+      (p) => `<tr>
+            <td>${p.name}</td>
+            <td>$ ${p.price}</td>
+            <td>
+              <img
+                src=\"${p.image}\"
+                alt=\"${p.name}\"
+                class=\"img-fluid rounded\"
+                style=\"width: 45px\"
+              />
+            </td>
+          </tr>`
+    ),
+  ].join(' ');
+  document.getElementById('products-table').innerHTML = html;
+});
+
+// socket.on('usersList', (users) => {
+//   const listUsers = Object.values(users)
+//     .map(
+//       (user) => `<li clase="m-1 border-button"> <strong>${user}</strong></li>`
+//     )
+//     .join(' ');
+
+//   document.getElementById('users-list').innerHTML = listUsers;
+// });
